79c9559af28df1624754db9c1a085619525a6414
Riday <ridayshah2001@gmail.com>
2020-08-15 13:20:33 +0530

    Fix: cache changes and aesthetics

--- api/index.js
+++ api/index.js
@@ -1,69 +1,57 @@
-require("dotenv").config();
-const {
-  renderError,
-  parseBoolean,
-  parseArray,
-  clampValue,
-  CONSTANTS,
-} = require("../src/common/utils");
-const fetchStats = require("../src/fetchers/stats-fetcher");
-const renderStatsCard = require("../src/cards/stats-card");
+require('dotenv').config();
+
+const { renderError, parseBoolean, parseArray } = require('../src/common/utils');
+const fetchStats = require('../src/fetchers/stats-fetcher');
+const renderStatsCard = require('../src/cards/stats-card');
 
 module.exports = async (req, res) => {
   const {
-    username,
-    hide,
-    hide_title,
-    hide_border,
-    hide_rank,
-    show_icons,
-    count_private,
-    include_all_commits,
-    line_height,
-    title_color,
-    icon_color,
-    text_color,
-    bg_color,
-    theme,
-    cache_seconds,
+	username,
+	hide,
+	hide_title,
+	hide_border,
+	hide_rank,
+	show_icons,
+	count_private,
+	include_all_commits,
+	line_height,
+	title_color,
+	icon_color,
+	text_color,
+	bg_color,
+	theme
   } = req.query;
   let stats;
 
-  res.setHeader("Content-Type", "image/svg+xml");
-
   try {
-    stats = await fetchStats(
-      username,
-      parseBoolean(count_private),
-      parseBoolean(include_all_commits)
-    );
+	stats = await fetchStats(
+	  username,
+	  parseBoolean(count_private),
+	  parseBoolean(include_all_commits)
+	);
+
+	if (username === 'ridays2001') res.setHeader('Cache-Control', 'public, max-age=900');
+	else res.setHeader('Cache-Control', `public, max-age=86400`);
+
+	res.setHeader('Content-Type', 'image/svg+xml');
+
+	return res.send(
+	  renderStatsCard(stats, {
+		hide: parseArray(hide),
+		show_icons: parseBoolean(show_icons),
+		hide_title: parseBoolean(hide_title),
+		hide_border: parseBoolean(hide_border),
+		hide_rank: parseBoolean(hide_rank),
+		include_all_commits: parseBoolean(include_all_commits),
+		line_height,
+		title_color,
+		icon_color,
+		text_color,
+		bg_color,
+		theme,
+	  })
+	);
   } catch (err) {
-    return res.send(renderError(err.message, err.secondaryMessage));
+	return res.send(renderError(err.message, err.secondaryMessage));
   }
-
-  const cacheSeconds = clampValue(
-    parseInt(cache_seconds || CONSTANTS.THIRTY_MINUTES, 10),
-    CONSTANTS.THIRTY_MINUTES,
-    CONSTANTS.ONE_DAY
-  );
-
-  if (username === 'ridays2001') res.setHeader('Cache-Control', 'public, max-age=900');
-  else res.setHeader('Cache-Control', `public, max-age=${cacheSeconds}`);
-
-  res.send(
-    renderStatsCard(stats, {
-      hide: parseArray(hide),
-      show_icons: parseBoolean(show_icons),
-      hide_title: parseBoolean(hide_title),
-      hide_border: parseBoolean(hide_border),
-      hide_rank: parseBoolean(hide_rank),
-      include_all_commits: parseBoolean(include_all_commits),
-      line_height,
-      title_color,
-      icon_color,
-      text_color,
-      bg_color,
-      theme,
-    })
-  );
 };
